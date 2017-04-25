e85b439121dd28ae1fad8f335dedb94c9bed9ea1
juancarpio27 <juan.carpio01@gmail.com>
2017-04-25 15:04:45 -0500

    facebook

--- app.js
+++ app.js
@@ -122,8 +122,8 @@ app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' })
 // handle the callback after facebook has authenticated the user
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', {
-        successRedirect: '/',
-        failureRedirect: '/'
+        successRedirect: 'http://google.com',
+        failureRedirect: 'http://google.com'
     }));
 
 app.use(passport.initialize());
